f413d9ed20b49cc3d2d1e43351abd6ffba308e57
HectorGerardoZV <torrozapata@gmail.com>
2022-09-16 23:35:14 -0700

    Majors model and router were added

--- src/routes/routes.routes.js
+++ src/routes/routes.routes.js
@@ -3,10 +3,12 @@ const usersRouter = require('./users.routes');
 const rolesRouter = require('./roles.routes');
 const profilesRouter = require('./profiles.routes');
 const vacanciesRouter = require('./vacancies.routes');
+const majorsRouter = require('./majors.routes')
 
 router.use(usersRouter);
 router.use(rolesRouter);
 router.use(profilesRouter);
 router.use(vacanciesRouter);
+router.use(majorsRouter);
 
 module.exports = router;
