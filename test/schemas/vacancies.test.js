42fc343eab239ec663523706bfbcb53c4fbc4216
HectorGerardoZV <torrozapata@gmail.com>
2022-10-29 22:42:41 -0700

    Majors and vacancies schemas test were modified to implement the new atributes

--- test/schemas/vacancies.test.js
+++ test/schemas/vacancies.test.js
@@ -3,7 +3,8 @@ const { VacanciesSchema } = require("../../src/schemas");
 describe("Testing Vacancies schema", () => {
     test("The vacancies schema must have a good structure. ", () => {
         const keys = VacanciesSchema.schema.obj;
-        console.log(keys);
+        expect(keys.hasOwnProperty("name")).toBe(true);
+        expect(keys.hasOwnProperty("major")).toBe(true);
         expect(keys.hasOwnProperty("company")).toBe(true);
         expect(keys.hasOwnProperty("created")).toBe(true);
         expect(keys.hasOwnProperty("generalData")).toBe(true);
@@ -12,6 +13,6 @@ describe("Testing Vacancies schema", () => {
         expect(keys.hasOwnProperty("requirements")).toBe(true);
         expect(keys.hasOwnProperty("limit")).toBe(true);
         expect(keys.hasOwnProperty("activated")).toBe(true);
-        expect(Object.keys(keys).length).toBe(8);
+        expect(Object.keys(keys).length).toBe(10);
     });
 });
