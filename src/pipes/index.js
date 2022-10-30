9d2bc95a63214ac384ef01e0545a591332585267
HectorGerardoZV <torrozapata@gmail.com>
2022-10-29 22:41:44 -0700

    majors, roles and users pipelines were created and integred

--- src/pipes/index.js
+++ src/pipes/index.js
@@ -1,7 +1,11 @@
 const vacanciesPipeLines = require("./vacancies.pipes");
 const majorsPipeLines = require("./majors.pipes");
+const rolesPipelines = require("./roles.pipes");
+const usersPipeLines = require("./users.pipes");
 
 module.exports = {
     vacanciesPipeLines,
     majorsPipeLines,
+    rolesPipelines,
+    usersPipeLines
 };
