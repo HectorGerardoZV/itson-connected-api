c1a39810cd757e9584e4f966e91c9d5390d57f74
HectorGerardoZV <torrozapata@gmail.com>
2022-10-29 22:43:55 -0700

    Majors and roles controllers were created, users controller was modified to implement LogError persistence

--- src/controllers/majors.controller.js
+++ src/controllers/majors.controller.js
@@ -34,7 +34,65 @@ const getAllMajors = async (req, res, next) => {
     }
 };
 
+const getMajorById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const major = await MajorsSchema.findOne({ _id: id });
+        if (!major) return res.status(404).json({ msg: "This major doesn't exist" });
+        res.status(200).json(major);
+    } catch (error) {
+        const errorLog = {
+            method: req.method,
+            url: req.url,
+            resource: "majors",
+            message: error.message,
+        };
+        req.errorLog = errorLog;
+        next();
+    }
+};
+
+const updateMajorById = async (req, res, next) => {
+    try {
+        const { body } = req;
+        const { id } = req.params;
+        const majorUpdated = await MajorsSchema.findOneAndUpdate({ _id: id }, body, { new: true });
+        if (!majorUpdated) return res.status(404).json({ msg: "This major doesn't exist" });
+        res.status(200).json(majorUpdated);
+    } catch (error) {
+        const errorLog = {
+            method: req.method,
+            url: req.url,
+            resource: "majors",
+            message: error.message,
+        };
+        req.errorLog = errorLog;
+        next();
+    }
+};
+
+const deleteMajorById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const majorDeleted = await MajorsSchema.findOneAndDelete({ _id: id });
+        if (!majorDeleted) res.status(404).json({ msg: "This major desn't exist" });
+        res.status(200).json({ msg: `The major ${majorDeleted.name} was deleted` });
+    } catch (error) {
+        const errorLog = {
+            method: req.method,
+            url: req.url,
+            resource: "majors",
+            message: error.message,
+        };
+        req.errorLog = errorLog;
+        next();
+    }
+};
+
 module.exports = {
     addNewMajor,
     getAllMajors,
+    getMajorById,
+    updateMajorById,
+    deleteMajorById,
 };
--- src/controllers/roles.controller.js
+++ src/controllers/roles.controller.js
@@ -1,27 +1,41 @@
 const { RolesSchema } = require("../schemas");
 
-const addNewRole = async (req, res) => {
+const addNewRole = async (req, res, next) => {
     try {
         const role = new RolesSchema(req.body);
         const roleCreated = await role.save();
         res.status(200).json(roleCreated);
     } catch (error) {
-        res.status(500).json({ msg: error.message });
+        const errorLog = {
+            method: req.method,
+            url: req.url,
+            resource: "roles",
+            message: error.message,
+        };
+        req.errorLog = errorLog;
+        next();
     }
 };
 
-const getRoleById = async (req, res) => {
+const getRoleById = async (req, res, next) => {
     try {
         const { idRole } = req.params;
         const role = await RolesSchema.findOne({ _id: idRole });
         if (!role) return res.status(404).json({ msg: "This role doesn't exist" });
         res.status(200).json(role);
     } catch (error) {
-        res.status(500).json({ msg: error.message });
+        const errorLog = {
+            method: req.method,
+            url: req.url,
+            resource: "roles",
+            message: error.message,
+        };
+        req.errorLog = errorLog;
+        next();
     }
 };
 
-const updateRoleById = async (req, res) => {
+const updateRoleById = async (req, res, next) => {
     try {
         const { idRole } = req.params;
         const roleUpdated = await RolesSchema.findOneAndUpdate({ _id: idRole }, req.body, {
@@ -30,18 +44,32 @@ const updateRoleById = async (req, res) => {
         if (!roleUpdated) return res.status(404).json({ msg: "This role doesn't exist" });
         res.status(200).json(roleUpdated);
     } catch (error) {
-        res.status(500).json({ msg: error.message });
+        const errorLog = {
+            method: req.method,
+            url: req.url,
+            resource: "roles",
+            message: error.message,
+        };
+        req.errorLog = errorLog;
+        next();
     }
 };
 
-const deleteRoleById = async (req, res) => {
+const deleteRoleById = async (req, res, next) => {
     try {
         const { idRole } = req.params;
         const roleDeleted = await RolesSchema.findOneAndDelete({ _id: idRole });
         if (!roleDeleted) return res.status(404).json({ msg: "This role doesn't exist" });
         return res.status(200).json({ msg: "Role deleted" });
     } catch (error) {
-        res.status(500).json({ msg: error.message });
+        const errorLog = {
+            method: req.method,
+            url: req.url,
+            resource: "roles",
+            message: error.message,
+        };
+        req.errorLog = errorLog;
+        next();
     }
 };
 
@@ -50,7 +78,14 @@ const getAllRoles = async (__req, res) => {
         const allRoles = await RolesSchema.find({});
         res.status(200).json(allRoles);
     } catch (error) {
-        res.status(500).json({ msg: error.message });
+        const errorLog = {
+            method: req.method,
+            url: req.url,
+            resource: "roles",
+            message: error.message,
+        };
+        req.errorLog = errorLog;
+        next();
     }
 };
 
